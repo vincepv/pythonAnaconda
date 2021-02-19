1636db9fd82e96755988b001ad8812b0a36091de
vincepv <vincentpaulvictor@gmail.com>
2021-02-19 13:01:47 +0100

    Delete merge_excel_sheet.py

--- merge_excel_sheet.py
+++
@@ -1,6 +0,0 @@
-#merge all sheets of one excel in one csv
-import pandas as pd
-
-def merge_excel_sheet(excel_file):
-	df = pd.concat(pd.read_excel(excel_file, sheet_name=None), ignore_index=True)
-	df.to_csv('/Users/VPV/Desktop/merge_excel_sheet.csv')
